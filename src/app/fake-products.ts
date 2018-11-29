536a39496256ce584338f96617ab5758ae8b51ad
klusm <Mateusz.Kluska@pega.com>
2018-11-29 21:37:16 +0000

    Added shop bootstrap template

--- src/app/fake-products.ts
+++ src/app/fake-products.ts
@@ -7,35 +7,35 @@ export class FakeProducts {
       name: 'Produkt1',
       products_number: 10,
       price: 1,
-      description: 'dfds',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur',
       photo_url: 'https://devducks.com/static/devducks/imgs/js-duck.jpg'
     },
     {
       name: 'Produkt2',
       products_number: 10,
       price: 1,
-      description: 'dfds',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur',
       photo_url: 'https://devducks.com/static/devducks/imgs/js-duck.jpg'
     },
     {
       name: 'Produkt3',
       products_number: 10,
       price: 1,
-      description: 'dfds',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur',
       photo_url: 'https://devducks.com/static/devducks/imgs/js-duck.jpg'
     },
     {
       name: 'Produkt4',
       products_number: 10,
       price: 1,
-      description: 'dfds',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur',
       photo_url: 'https://devducks.com/static/devducks/imgs/js-duck.jpg'
     },
     {
       name: 'Produkt5',
       products_number: 10,
       price: 1,
-      description: 'dfds',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur',
       photo_url: 'https://devducks.com/static/devducks/imgs/js-duck.jpg'
     }
   ];
